518e977f45736e0e466b212d4e1ac1b2c5ec0a40
aethena <divyakali1288@gmail.com>
2015-04-04 15:55:26 -0400

    adding final commit

--- src/games/memory.py
+++ src/games/memory.py
@@ -2,26 +2,50 @@
 
 import simplegui
 import random
+card = 0
 deck = []
-pos_init = [80,0]
+CARD_WIDTH = 80
 exposed =[]
+CANVAS_WIDTH = 1280 
+CANVAS_HEIGHT = 100
+state = 0
+turns=0
+exposed_card_numbers =[]
+
 # helper function to initialize globals
 def new_game():
-    global deck
-    global exposed
+    global deck,exposed,state,turns
+    state =0
+    turns =0
     deck = range(8)
     deck += deck
-    exposed=[True]*len(deck)
-    exposed[3]=False
-    exposed[9]=False
+    exposed=[False]*len(deck)
     random.shuffle(deck)
-    print exposed
 
-     
 # define event handlers
 def mouseclick(pos):
-    # add game state logic here
-    pass
+    global state,exposed_card_numbers,turns
+    card = (pos[0]//CARD_WIDTH)
+    if (not exposed[card]):
+        exposed[card]=True
+        
+        if (state == 0):
+            state = 1
+            turns +=1
+            label.set_text("Turns="+str(turns))
+            exposed_card_numbers.append(card)
+        elif (state==1):
+            state = 2
+            exposed_card_numbers.append(card)
+        elif (state==2):
+            state = 1
+            if (deck[exposed_card_numbers[1]] != deck[exposed_card_numbers[0]]):
+                exposed[exposed_card_numbers[1]] = False
+                exposed[exposed_card_numbers[0]] = False
+            exposed_card_numbers=[]
+            exposed_card_numbers.append(card)
+            turns +=1
+            label.set_text("Turns="+str(turns))
     
                         
 # cards are logically 50x100 pixels in size    
@@ -34,20 +58,17 @@ def draw(canvas):
         else:
             txt_pos = key
         if(exposed[key]):
-            canvas.draw_text(str(deck[key]), [30+((key)*pos_init[0]),50], 40, 'Red')
+            canvas.draw_text(str(deck[key]), [30+((key)*CARD_WIDTH),CANVAS_HEIGHT/2], 40, 'White')
         else:
-            canvas.draw_polygon([[key*pos_init[0],0],\
-                                 [key*pos_init[0],100],\
-                                 [(key+1)*pos_init[0],100],\
-                                 [(key+1)*pos_init[0],0]], 3,"black", "Green")
-    
-    
-
+            canvas.draw_polygon([[key*CARD_WIDTH,0],\
+                                 [key*CARD_WIDTH,CANVAS_HEIGHT],\
+                                 [(key+1)*CARD_WIDTH,CANVAS_HEIGHT],\
+                                 [(key+1)*CARD_WIDTH,0]], 3,"Black", "Green")
 
 # create frame and add a button and labels
-frame = simplegui.create_frame("Memory", 1200, 100)
+frame = simplegui.create_frame("Memory", CANVAS_WIDTH, CANVAS_HEIGHT )
 frame.add_button("Reset", new_game)
-label = frame.add_label("Turns = 0")
+label = frame.add_label("Turns="+str(turns))
 
 # register event handlers
 frame.set_mouseclick_handler(mouseclick)
@@ -58,4 +79,3 @@ new_game()
 frame.start()
 
 
-# Always remember to review the grading rubric
